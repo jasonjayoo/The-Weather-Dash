7dbcbed4cbe0be93470e19e5d493753f8f1ccf05
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-01-25 14:29:02 -0800

    working on button to retrieve city data

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ var day5Humid = document.querySelector(".humid5");
 
 // var currentWeatherEl = document.getElementById('#currentWeather');
 
+
 var savedCities = JSON.parse(localStorage.getItem("citySearchHistory")) || [];
 
 searchBtn.addEventListener("click", function () {
@@ -66,6 +67,7 @@ searchBtn.addEventListener("click", function () {
   localStorage.setItem("citySearchHistory", JSON.stringify(savedCities));
 
   getWeatherInfo(searchedCity);
+//   futureForecast(searchedCity) 
   createSearchHistory();
 });
 
@@ -73,7 +75,12 @@ searchBtn.addEventListener("click", function () {
 // var searchedCityList = localStorage.getItem("citySearchHistory");
 // searchHistory.append(JSON.parse(searchedCityList));
 
-// local storage not working!!!
+
+$('.day1').text(moment().add(1, 'days').format("L"));
+$('.day2').text(moment().add(2, 'days').format("L"));
+$('.day3').text(moment().add(3, 'days').format("L"));
+$('.day4').text(moment().add(4, 'days').format("L"));
+$('.day5').text(moment().add(5, 'days').format("L"));
 
 var apiKey = "bea1f09c5d5807a4bcd9c22e60cd333d";
 
@@ -84,12 +91,16 @@ function getWeatherInfo(city) {
     "&appid=" +
     apiKey;
 
+    console.log(city)
+
   fetch(queryURL1)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       //console.log(data);
+      searchbar.value = "";
+      
       var showName = data.name;
 
       mainDisplayCity.textContent = showName;
@@ -104,7 +115,7 @@ function getWeatherInfo(city) {
           return res.json();
         })
         .then(function (data) {
-        //   console.log(data);
+          console.log(data);
 
           var icon = data.current.weather[0].icon
 
@@ -112,7 +123,7 @@ function getWeatherInfo(city) {
 
           mainTemp.textContent = data.current.temp + " °F";
           mainWindspeed.textContent = data.current.wind_speed + " MPH";
-          mainHumid.textContent = data.current.humidity + " %";
+          mainHumid.textContent = data.current.humidity + "%";
 
           mainUV.textContent = data.current.uvi;
 
@@ -136,26 +147,106 @@ function getWeatherInfo(city) {
                 mainUV.style.padding = "0px 5px 0px 5px"
                 mainUV.style.borderRadius = "5px"
             } 
+
+        var icon1 = data.daily[1].weather[0].icon
+        var icon2 = data.daily[2].weather[0].icon
+        var icon3 = data.daily[3].weather[0].icon
+        var icon4 = data.daily[4].weather[0].icon
+        var icon5 = data.daily[5].weather[0].icon
+
+        day1Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon1}.png`);
+        
+        day2Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon2}.png`);
+        
+        day3Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon3}.png`);
+        
+        day4Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon4}.png`);
+        
+        day5Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon5}.png`);
+
+    
+        day1Temp.textContent = data.daily[1].temp.max + " °F";
+        day2Temp.textContent = data.daily[2].temp.max+ " °F";
+        day3Temp.textContent = data.daily[3].temp.max + " °F";
+        day4Temp.textContent = data.daily[4].temp.max + " °F";
+        day5Temp.textContent = data.daily[5].temp.max + " °F";
+        
+
+        day1WS.textContent = data.daily[1].wind_speed + " MPH";
+        day2WS.textContent = data.daily[2].wind_speed + " MPH";
+        day3WS.textContent = data.daily[3].wind_speed + " MPH";
+        day4WS.textContent = data.daily[4].wind_speed + " MPH";
+        day5WS.textContent = data.daily[5].wind_speed + " MPH";
+
+        day1Humid.textContent = data.daily[1].humidity + "%";
+        day2Humid.textContent = data.daily[2].humidity + "%";
+        day3Humid.textContent = data.daily[3].humidity + "%";
+        day4Humid.textContent = data.daily[4].humidity + "%";
+        day5Humid.textContent = data.daily[5].humidity + "%";
           
         });
-
 
     });
 }
 
-function futureForecast() {
-    var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=bea1f09c5d5807a4bcd9c22e60cd333d";
 
-    $('.day1').text(moment().format("(L)"));
 
-    fetch(queryURL3)
-        .then(function (resp) {
-            return resp.json();
-        }) 
-        .then(function (data){
-            console.log(data)
-        })
-}
+// function futureForecast(city) {
+//     var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=bea1f09c5d5807a4bcd9c22e60cd333d";
+
+
+//     fetch(queryURL3)
+//         .then(function (resp) {
+//             return resp.json();
+//         }) 
+//         .then(function (data){
+//             console.log(data);
+        
+        // var icon1 = data.list[1].weather[0].icon
+        // var icon2 = data.list[2].weather[0].icon
+        // var icon3 = data.list[3].weather[0].icon
+        // var icon4 = data.list[4].weather[0].icon
+        // var icon5 = data.list[5].weather[0].icon
+
+        // day1Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon1}.png`);
+        
+        // day2Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon2}.png`);
+        
+        // day3Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon3}.png`);
+        
+        // day4Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon4}.png`);
+        
+        // day5Icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon5}.png`);
+
+    
+        // day1Temp.textContent = data.list[1].main.temp + " °F";
+        // day2Temp.textContent = data.list[2].main.temp + " °F";
+        // day3Temp.textContent = data.list[3].main.temp + " °F";
+        // day4Temp.textContent = data.list[4].main.temp + " °F";
+        // day5Temp.textContent = data.list[5].main.temp + " °F";
+        
+
+        // day1WS.textContent = data.list[1].wind.speed + " MPH";
+        // day2WS.textContent = data.list[2].wind.speed + " MPH";
+        // day3WS.textContent = data.list[3].wind.speed + " MPH";
+        // day4WS.textContent = data.list[4].wind.speed + " MPH";
+        // day5WS.textContent = data.list[5].wind.speed + " MPH";
+
+        // day1Humid.textContent = data.list[1].main.humidity + "%";
+        // day2Humid.textContent = data.list[2].main.humidity + "%";
+        // day3Humid.textContent = data.list[3].main.humidity + "%";
+        // day4Humid.textContent = data.list[4].main.humidity + "%";
+        // day5Humid.textContent = data.list[5].main.humidity + "%";
+
+
+
+        // day1Forecast.textContent = data.list[1].dt_txt.split(" ");
+        // for(var i = 0; i < data.list.length; i++)
+        // {
+        // console.log(data.list[i].dt_txt.split(" "));
+        // }
+//         })
+// }
 
 
 var searchHistoryList = [];
@@ -173,7 +264,8 @@ function createSearchHistory() {
     if (searchedCityList !== null) {
         searchHistoryList = searchedCityList;
     }
-    console.log(searchHistoryList) //double check the stuff in this array.
+    // console.log(searchHistoryList) 
+    //double check the stuff in this array.
     
     
     //2. create a button for each string, and put that in our div.
@@ -183,11 +275,21 @@ function createSearchHistory() {
         var buttonList = document.createElement("button");
         buttonList.textContent = citySearchList;
 
+        console.log(buttonList)
+
         // cityList.setAttribute("citySearchHistory", i);
         // console.log(buttonList)
 
         //you should also give buttonList an event listener here, before appending.
         buttonList.addEventListener("click",function(e){
+        //   searchbar.value = buttonList.textContent
+     console.log(cityList[i])
+          getWeatherInfo();
+console.log(citySearchList)
+console.log(searchHistoryList)
+
+//   futureForecast(searchedCity) 
+//   createSearchHistory();
           console.log("You clicked on ")
           console.log(e.target)
         })
